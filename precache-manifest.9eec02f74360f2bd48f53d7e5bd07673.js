8f759505a9a89aa6f39c76ee94ae950a905c06cb
Rohan <rohancalgary14@gmail.com>
2019-11-13 13:49:10 -0500

    updated website

---
+++ precache-manifest.9eec02f74360f2bd48f53d7e5bd07673.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "863a32b1f5dd6669eb94b1384f84926e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58e017ef2c31bd001493",
+    "url": "/static/css/main.b996bd57.chunk.css"
+  },
+  {
+    "revision": "1eae4a68755cb18df81e",
+    "url": "/static/js/2.53b104f9.chunk.js"
+  },
+  {
+    "revision": "58e017ef2c31bd001493",
+    "url": "/static/js/main.cb3cf200.chunk.js"
+  },
+  {
+    "revision": "a444faff98137cbc7e9f",
+    "url": "/static/js/runtime-main.fdd9b878.js"
+  },
+  {
+    "revision": "09bccbfcb6b8a6425e90786b7bac436c",
+    "url": "/static/media/stratum.09bccbfc.ttf"
+  }
+]);
